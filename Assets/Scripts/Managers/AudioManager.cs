b583c3af9e83ff648b01cd100eb584266eb15659
DivijManik <51446479+DivijManik@users.noreply.github.com>
2022-04-10 15:57:19 +0530

    Music Added

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    private void Awake()
+    {
+        if(Instance== null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+}
