6e040b23e66d9b0c175e6df51e5eb57507972735
DivijManik <51446479+DivijManik@users.noreply.github.com>
2022-08-25 01:54:23 +0530

    Huge Improvement - scale Obj
    * level 30 to 40 added
    * Continuous rotation added
    * scale spikes (Pointy cones
    * and huge improvements done

--- Assets/Scripts/ObjRotattionScript.cs
+++ Assets/Scripts/ObjRotattionScript.cs
@@ -7,10 +7,59 @@ public class ObjRotattionScript : MonoBehaviour
     [SerializeField]
     Vector3[] EulerAngles;
 
+    int dir = 1;
+    int speed = 100;
+
+    [SerializeField]
+    RotationType rotationType;
+
+    [SerializeField]
+    RotationAxis rotationAxis;
+
     void Start()
     {
-        transform.eulerAngles = EulerAngles[Random.Range(0, EulerAngles.Length)];
+        if (rotationType == RotationType.RandomEulAngles)
+        {
+            transform.eulerAngles = EulerAngles[Random.Range(0, EulerAngles.Length)];
+        }
+        else
+        {
+            dir = Random.Range(0, 2) == 0 ? -1 : 1;
+            speed = Random.Range(100, 150);
+        }
     }
+
+    private void FixedUpdate()
+    {
+        if (rotationType == RotationType.Continious)
+        {
+            if (rotationAxis == RotationAxis.Z)
+            {
+                transform.Rotate(0, 0, (speed * dir) * Time.deltaTime);
+            }
+            else if(rotationAxis == RotationAxis.Y)
+            {
+                transform.Rotate(0, (speed * dir) * Time.deltaTime, 0);
+            }
+            else // X
+            {
+                transform.Rotate( (speed * dir) * Time.deltaTime, 0, 0);
+            }
+        }
+    }
+}
+
+enum RotationType
+{
+    RandomEulAngles,
+    Continious
+}
+
+enum RotationAxis
+{
+    X,
+    Y,
+    Z
 }
 
 
---
+++ Assets/Scripts/ObjScaleScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+public class ObjScaleScript : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] ObjsToScale;
+
+    [SerializeField]
+    Vector3 ScaleValue;
+
+    private void Awake()
+    {
+        foreach (Transform item in ObjsToScale)
+        {
+            item.localScale = Vector3.zero;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<CollisionDetect>() != null)
+        {
+            foreach (Transform item in ObjsToScale)
+            {
+                item.DOScale(ScaleValue, 0.5f);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<CollisionDetect>() != null)
+        {
+            foreach (Transform item in ObjsToScale)
+            {
+                item.localScale = Vector3.zero;
+            }
+        }
+    }
+
+}
